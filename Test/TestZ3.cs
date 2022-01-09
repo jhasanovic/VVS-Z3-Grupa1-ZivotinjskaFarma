be5a745e540315b1d9757734348fa6d48c2d117a
shadzijusu1 <shadzijusu1@etf.unsa.ba>
2022-01-09 09:26:30 +0100

    change tests

--- Test/TestZ3.cs
+++ Test/TestZ3.cs
@@ -70,62 +70,7 @@ namespace Test
                 */
         //Testovi za potpuni obuhvat uslova
         //Selma Hadžijusufović
-        [TestMethod]
-        public void Test1()
-        {
-            //provjera datuma kupovine false
-            f.KupovinaProizvoda(p, System.DateTime.Now.AddDays(5), 10);
-            Assert.IsTrue(f.KupovinaProizvoda(p, System.DateTime.Now.AddDays(5), 10));
-
-        }
-        [TestMethod]
-        public void Test2()
-        {
-            //provjera datuma kupovine true, id false
-            DateTime vrijeme = DateTime.Now;
-            int id = 0;
-            Kupovina k1 = new Kupovina(id.ToString(), vrijeme, vrijeme.AddDays(5), p, 10, false);
-
-            f.Kupovine.Add(k1);
-
-            Assert.IsTrue(f.KupovinaProizvoda(p, System.DateTime.Now.AddDays(5), 10));
-
-        }
-        [TestMethod]
-        public void Test3()
-        {
-            //provjera datuma kupovine true, id true, kupljeni proizvod false
-            DateTime vrijeme = DateTime.Now;
-            List<string> parametri = new List<string>();
-            parametri.Add("Naziv");
-            parametri.Add("Adresa");
-            parametri.Add("12");
-            parametri.Add("Sarajevo");
-            parametri.Add("71000");
-            parametri.Add("Bosna i Hercegovina");
-            l = new Lokacija(parametri, 100);
-            z = new Zivotinja(ZivotinjskaVrsta.Kokoška, System.DateTime.Now.AddDays(-500), 100, 100, l);
-            Proizvod p1 = new Proizvod("", "", "Jaja", z, System.DateTime.Now.AddDays(-5), System.DateTime.Now.AddDays(5), 100);
-            int id = 2;
-            Kupovina k1 = new Kupovina(id.ToString(), vrijeme, vrijeme.AddDays(5), p1, 10, false);
-
-            f.Kupovine.Add(k1);
-            Assert.IsTrue(f.KupovinaProizvoda(p, System.DateTime.Now.AddDays(5), 10));
-
-        }
-        [TestMethod]
-        public void Test4()
-        {
-            //provjera datuma kupovine true, id true, kupljeni proizvod true
-            DateTime vrijeme = DateTime.Now;
-         
-            int id = 2;
-            Kupovina k1 = new Kupovina(id.ToString(), vrijeme, vrijeme.AddDays(5), p, 10, false);
-
-            f.Kupovine.Add(k1);
-            Assert.IsTrue(f.KupovinaProizvoda(p, System.DateTime.Now.AddDays(5), 10));
-
-        }
+        
 
 
     }
