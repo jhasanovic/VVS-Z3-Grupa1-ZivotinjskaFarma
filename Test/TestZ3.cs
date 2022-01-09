d1ce736a4dd85eb503cbb25737b56326b6593d27
shadzijusu1 <shadzijusu1@etf.unsa.ba>
2022-01-09 11:48:57 +0100

    testovi za obuhvat uslova

--- Test/TestZ3.cs
+++ Test/TestZ3.cs
@@ -70,7 +70,43 @@ namespace Test
                 */
         //Testovi za potpuni obuhvat uslova
         //Selma Hadžijusufović
-        
+        [TestMethod]
+        public void TestUslovi1()
+        {
+            //verificiraj kupovinu 
+            Assert.IsFalse(f.KupovinaProizvoda(p, System.DateTime.Now.AddDays(5), 55));
+}
+
+        [TestMethod]
+
+        public void TestUslovi2()
+        {
+            //Datumi kupovine različiti
+            List<string> parametri = new List<string>();
+            parametri.Add("Naziv");
+            parametri.Add("Adresa");
+            parametri.Add("12");
+            parametri.Add("Sarajevo");
+            parametri.Add("71000");
+            parametri.Add("Bosna i Hercegovina");
+            l = new Lokacija(parametri, 100);
+            z = new Zivotinja(ZivotinjskaVrsta.Kokoška, System.DateTime.Now.AddDays(-500), 100, 100, l);
+            p = new Proizvod("", "", "Jaja", z, System.DateTime.Now.AddDays(-5), System.DateTime.Now.AddDays(5), 100);
+            Kupovina kupovina = new Kupovina(1.ToString(), DateTime.Now, DateTime.Now.AddDays(5), p, 10, false);
+            f.Kupovine.Add(kupovina);
+            Assert.IsTrue(f.KupovinaProizvoda(p, System.DateTime.Now.AddDays(5), 10));
+        }
+        [TestMethod]
+
+        public void TestUslovi3()
+        {
+            //Nema zabilježenih kupovina, provjera uslova da je postojecaKupovina == null
+            Assert.IsTrue(f.KupovinaProizvoda(p, System.DateTime.Now.AddDays(5), 10));
+        }
+
+
+
+
 
 
     }
